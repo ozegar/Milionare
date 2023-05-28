1814b84d589f52d45ef050057cde28164e9f6ac0
Oskars Rutmanis <oskars.rutmanis@ldz.lv>
2023-05-28 23:46:37 +0300

    initial commit

---
+++ MainActivity.cs
@@ -0,0 +1,159 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using Microsoft.Data.Sqlite;
+using Milionare.Properties;
+using System;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+using static Java.Text.Normalizer;
+using static Xamarin.Essentials.Platform;
+
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+
+namespace Milionare
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+
+    // set globals
+    //    ((App) App.Current).ContinueOrNot = true;
+    public class MainActivity : AppCompatActivity
+    {
+        Button btnStart;
+        string connectionString;
+        string pathToDB;
+        string Jaut,A,B,C,D,Atb;
+        
+        readonly string[] Permission =
+        {
+            Android.Manifest.Permission.WriteExternalStorage
+
+        };
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            RequestPermissions(Permission, 1);
+            Tiesibas();
+            LinearLayout ll = new LinearLayout(this);
+            var lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MatchParent,
+                LinearLayout.LayoutParams.MatchParent);
+            btnStart = FindViewById<Button>(Resource.Id.btnStart);
+            btnStart.Click += delegate
+            {
+                //kopēt uz external un atvērt DB
+                var folders = this.GetExternalMediaDirs();
+                var FolderPath = folders[0].AbsolutePath;
+                pathToDB = Path.Combine(FolderPath, "million.db");
+                connectionString = $"Data Source={pathToDB}"; //bez ;
+                using (var source = Resources.OpenRawResource(Resource.Raw.million))
+                {
+                    using (var destination = File.Open(pathToDB, FileMode.Create))
+                    {
+                        source.CopyTo(destination);
+                    }
+                }
+                if (File.Exists(pathToDB))
+                {
+
+                    //Toast.MakeText(this, "Database allready exists", ToastLength.Long).Show();
+                    //string SQLDB= "select * from test where first_name like @param"
+                    //GetData("select * from test");
+                    //SELECT * FROM questansw where lvl=1 ORDER BY RANDOM() LIMIT 1;
+                    Android.Content.Intent frm = new Android.Content.Intent(this, typeof(QuestionActivity));
+                    for (var i = 1; i < 10 && Intent.GetStringExtra("Continue") != "false"; i++)
+                    {
+
+                        frm.PutExtra("filter", i.ToString());
+                        GetData("SELECT * FROM questansw where lvl=@lvl ORDER BY RANDOM() LIMIT 1;", "lvl", i.ToString());
+                        frm.PutExtra("jautajums", Jaut);
+                        frm.PutExtra("A", A);
+                        frm.PutExtra("B", B);
+                        frm.PutExtra("C", C);
+                        frm.PutExtra("D", D);
+                        frm.PutExtra("Atb", Atb);
+                        StartActivity(frm);
+                        if (Intent.GetStringExtra("Continue") == "false")  //Intent.GetBooleanExtra("Continue", true) == false
+                        {
+                            Toast.MakeText(this, "Spēles beigas parādam rezultātu formu", ToastLength.Long).Show();
+                            //end
+                            //return;
+                        }
+                    }
+                    
+                    return; //Database file already exists in the external storage
+                }
+
+                
+
+            };
+
+
+        }
+
+        public void GetData(string SQL, string ParamName, string ParamValue)   //, string ParamName, string ParamValue
+        {
+            string data = "";
+            try
+            {
+                using (var dbConn = new SqliteConnection(connectionString))
+                {
+                    dbConn.Open();
+                    using (SqliteCommand cmd = new SqliteCommand(SQL, dbConn))
+                    {
+                        //cmd.ExecuteNonQuery();
+                        //cmd.Parameters.AddWithValue("param", "U%");
+                        cmd.Parameters.AddWithValue(ParamName, ParamValue);
+                        using (var dr = cmd.ExecuteReader())
+                        {
+                            while (dr.Read())
+                            {
+                                //var fieldcount = dr.FieldCount;
+                                //for(int i = 0; i < fieldcount; i++)
+                                //{
+                                //    data += dr[i] + "";
+                                //}
+                                //txtData.Text = data + "\n";
+                                //data = "";
+
+                                //txtData.Text += dr[0].ToString() + dr[1].ToString() +System.Environment. ...ToString()?
+                                //txtData += $"{dr[0].ToString()} {dr[1].ToString()} \n";
+                                Jaut = dr[1].ToString();
+                                A = dr[2].ToString();
+                                B = dr[3].ToString();
+                                C = dr[4].ToString();
+                                D = dr[5].ToString();
+                                Atb = dr[6].ToString();
+                                // System.Console.WriteLine(dr[2].ToString());
+                            }
+                        }
+                    }
+                    dbConn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Toast.MakeText(this, ex.Message, ToastLength.Long).Show();
+            }
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private void Tiesibas()
+        {
+            if (CheckSelfPermission(Android.Manifest.Permission.WriteExternalStorage)
+                != Android.Content.PM.Permission.Granted)
+            {
+                RequestPermissions(Permission, 0);
+            }
+        }
+    }
+}
---
+++ QuestionActivity.cs
@@ -0,0 +1,120 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Icu.Text;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using static Java.Text.Normalizer;
+
+namespace Milionare.Properties
+{
+    [Activity(Label = "QuestionActivity")]
+    public class QuestionActivity : Activity
+    {
+        TextView txtJautView;
+        Button btnA, btnB, btnC, btnD;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            LinearLayout ll = new LinearLayout(this);
+            var lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MatchParent,
+                LinearLayout.LayoutParams.MatchParent);
+            SetContentView(Resource.Layout.Jautajumi);
+            btnA = FindViewById<Button>(Resource.Id.btnA);
+            btnB = FindViewById<Button>(Resource.Id.btnB);
+            btnC = FindViewById<Button>(Resource.Id.btnC);
+            btnD = FindViewById<Button>(Resource.Id.btnD);
+            txtJautView = FindViewById<TextView>(Resource.Id.txtJautView);
+            btnA.Visibility = ViewStates.Visible;
+            btnB.Visibility = ViewStates.Visible;
+            btnC.Visibility = ViewStates.Visible;
+            btnD.Visibility = ViewStates.Visible;
+            txtJautView.Text = Intent.GetStringExtra("jautajums");
+            btnA.Text = Intent.GetStringExtra("A");
+            btnB.Text = Intent.GetStringExtra("B");
+            btnC.Text = Intent.GetStringExtra("C");
+            btnD.Text = Intent.GetStringExtra("D");
+            btnA.Click += delegate
+            {
+                CheckButton("1");
+                return;
+            };
+            btnB.Click += delegate
+            {
+                CheckButton("2");
+                return;
+            };
+            btnC.Click += delegate
+            {
+                CheckButton("3");
+                return;
+            };
+            btnD.Click += delegate
+            {
+                CheckButton("4");
+                return;
+            };
+            // Create your application here
+        }
+
+        private void CheckButton(string poga)
+        {
+            if (poga == Intent.GetStringExtra("Atb"))
+            {   //pareiza atbilde
+
+                switch (poga)
+                {       ////SetBackgroundColor(Android.Graphics.Color.Green);
+                    case "1":
+                        btnA.Background.SetColorFilter(Android.Graphics.Color.Orange, Android.Graphics.PorterDuff.Mode.Multiply);
+                        break;
+                    case "2":
+                        btnB.Background.SetColorFilter(Android.Graphics.Color.Orange, Android.Graphics.PorterDuff.Mode.Multiply);
+                        break;
+                    case "3":
+                        btnC.Background.SetColorFilter(Android.Graphics.Color.Orange, Android.Graphics.PorterDuff.Mode.Multiply);
+                        break;
+                    case "4":
+                        btnD.Background.SetColorFilter(Android.Graphics.Color.Orange, Android.Graphics.PorterDuff.Mode.Multiply);
+                        break;
+                    default:
+                        Toast.MakeText(this, "Atbilde ir pareiza", ToastLength.Long).Show();
+                        break;
+                }
+                Finish();
+                //ja dialog logā yes un tā ir pareiza atbilde
+                //switch (poga)
+                //{       ////SetBackgroundColor(Android.Graphics.Color.Green);
+                //    case "1":
+                //        btnA.Background.SetColorFilter(Android.Graphics.Color.Green, Android.Graphics.PorterDuff.Mode.Multiply);
+                //        break;
+                //    case "2": 
+                //        btnB.Background.SetColorFilter(Android.Graphics.Color.Green, Android.Graphics.PorterDuff.Mode.Multiply);
+                //        break;
+                //    case "3": 
+                //        btnC.Background.SetColorFilter(Android.Graphics.Color.Green, Android.Graphics.PorterDuff.Mode.Multiply);
+                //        break;
+                //    case "4": 
+                //        btnD.Background.SetColorFilter(Android.Graphics.Color.Green, Android.Graphics.PorterDuff.Mode.Multiply);
+                //        break;
+                //    default:    Toast.MakeText(this, "Atbilde ir pareiza", ToastLength.Long).Show();
+                //        break;
+                //}
+            }
+            else
+            {   //kļūdaina atbilde - spēle zaudēta
+                Toast.MakeText(this, "kļūdaina atbilde", ToastLength.Long).Show();
+                Android.Content.Intent frm = new Android.Content.Intent(this, typeof(MainActivity));
+                frm.PutExtra("Continue", "false");
+                Finish();
+                SetContentView(Resource.Layout.activity_main);
+            }
+        }
+    }
+}
